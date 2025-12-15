f022c793773285c2f06747a255014875606726ec
omar-shandaq <omar.shandaq@hotmail.com>
2025-12-15 14:01:55 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
     });
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:streamGenerateContent?key=${GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:GenerateContent?key=${GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
